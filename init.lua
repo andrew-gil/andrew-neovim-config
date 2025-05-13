f83dc0f4718fff91ab39f5091567be84d3e2f053
Andrew Gil <andrewsebing@gmail.com>
2025-05-12 21:16:35 -0400

    update telescope

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 --Plug('OmniSharp/omnisharp-vim')
 Plug('nvim-lua/plenary.nvim')
-Plug('nvim-telescope/telescope.nvim', { ['tag'] = '0.1.8' })
+Plug('nvim-telescope/telescope.nvim')
 Plug('nvim-telescope/telescope-ui-select.nvim')
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
 Plug('neovim/nvim-lspconfig')
