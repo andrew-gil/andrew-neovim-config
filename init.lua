6d21f7a64857fd2b40729e227aa33824ed8b3474
Andrew Gil <andrewsebing@gmail.com>
2025-05-12 21:39:36 -0400

    installing omnisharp through vim plug, need :OmniSharpInstall

--- init.lua
+++ init.lua
@@ -3,23 +3,21 @@ local vim = vim
 local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
---Plug('OmniSharp/omnisharp-vim')
--- Language Servers
+-- Language Server related plugins
 Plug('neovim/nvim-lspconfig')
 Plug('hrsh7th/cmp-nvim-lsp')
 Plug('hrsh7th/nvim-cmp')
 Plug('hrsh7th/cmp-buffer')
 Plug('hrsh7th/vim-vsnip')
 Plug('hrsh7th/cmp-vsnip')
---Plug('mason-org/mason.nvim')
---Plug('mason-org/mason-lspconfig.nvim')
-Plug('norcalli/nvim-colorizer.lua')
-Plug('sindrets/diffview.nvim')
+Plug('OmniSharp/omnisharp-vim')
 -- general plugins
 Plug('nvim-lua/plenary.nvim') --prereq for telescope
 Plug('nvim-telescope/telescope.nvim') -- for searching and such
 Plug('nvim-telescope/telescope-ui-select.nvim') -- for applying telescope ui to code actions
 Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'}) -- treesitter
+Plug('sindrets/diffview.nvim') -- for viewing git diff
+Plug('norcalli/nvim-colorizer.lua') -- for viewing hex colors while editing
 -- color schemes
 Plug('morhetz/gruvbox')
 Plug('lifepillar/vim-solarized8')
