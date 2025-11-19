ad88ee82ad90d8f9f4a9ab9cf1b5defebee540d1
agil <andrewsebing@gmail.com>
2025-11-19 07:15:17 -0500

    removing dotnet test, coerce user to use makeprg for that

--- lua/make.lua
+++ lua/make.lua
@@ -6,6 +6,8 @@ vim.api.nvim_create_autocmd("FileType", {
   desc = "Set dotnet compiler for C# files"
 })
 
+-- to have a quickfix workflow for dotnet test, you can do :set makeprg=dotnet test
+
 -- Filter quickfix list to only show errors, not warnings
 vim.api.nvim_create_autocmd("QuickFixCmdPost", {
   pattern = "make",
@@ -21,38 +23,4 @@ vim.api.nvim_create_autocmd("QuickFixCmdPost", {
     end
   end,
   desc = "Filter dotnet warnings from quickfix list"
-})
-
--- Run dotnet test and populate quickfix with failures
-local function dotnet_test()
-  -- Save the errorformat
-  local saved_efm = vim.o.errorformat
-
-  -- Set errorformat for dotnet test output
-  -- Format: "  at Namespace.Class.Method() in /path/file.cs:line 123"
-  vim.o.errorformat = '%E%*[\\ ]at%.%#in %f:line %l,%Z%*[\\ ]%m,%C%m,%-G%.%#'
-
-  -- Run dotnet test and capture output
-  vim.cmd('cexpr system("dotnet test 2>&1")')
-
-  -- Restore errorformat
-  vim.o.errorformat = saved_efm
-
-  -- Open quickfix window
-  vim.cmd('copen')
-end
-
--- Create command for running dotnet test
-vim.api.nvim_create_user_command('DotnetTest', dotnet_test, {
-  desc = 'Run dotnet test and populate quickfix with failures'
-})
-
--- Optionally set makeprg for test in C# files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "cs",
-  callback = function()
-    -- You can use :make! (with bang) to run tests instead of build
-    -- vim.bo.makeprg = 'dotnet test'
-  end,
-  desc = "Configure dotnet test for C# files"
 })
